52c6f548264fc593b4b5d11f5c79fb8e4b08615f
MaiiaHa <maiia.robeiko@ukr.net>
2023-05-19 14:37:48 +0200

    finalizing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// ========= hooks =========================
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import Container from './Container/Container';
@@ -27,11 +26,11 @@ export default function App() {
       alert(`${name} is already in list. Please enter other name.`);
       return;
     }
-    setContacts([newUserData, ...contacts]);
+
+    setContacts(contacts => [newUserData, ...contacts]);
   };
 
   const hendleChangeFilter = e => {
-    e.preventDefault(e);
     setFilter(e.target.value);
   };
 
@@ -62,100 +61,3 @@ export default function App() {
     </div>
   );
 }
-//=============================================
-// import React, { Component } from 'react';
-// import { nanoid } from 'nanoid';
-// import Container from './Container/Container';
-// import ContactForm from './ContactForm/ContactForm';
-// import Filter from './Filter/Filter';
-// import ContactList from './ContactList/ContactList';
-
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   addContact = ({ name, number }) => {
-//     const newUserData = {
-//       id: nanoid(10),
-//       name,
-//       number,
-//     };
-
-//     const checkedName = this.state.contacts.find(
-//       ({ name }) => name === newUserData.name
-//     );
-//     if (checkedName) {
-//       alert(`${name} is already in list. Please enter other name.`);
-//       return;
-//     }
-//     this.setState(prevState => ({
-//       contacts: [newUserData, ...prevState.contacts],
-//     }));
-//   };
-
-//   hendleChangeFilter = event => {
-//     event.preventDefault(event);
-//     this.setState({ filter: event.currentTarget.value });
-//   };
-
-//   deleteContact = userId => {
-//     this.setState(({ contacts }) => ({
-//       contacts: contacts.filter(({ id }) => userId !== id),
-//     }));
-//   };
-
-//   getVisibleContacts = () => {
-//     const { filter, contacts } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   render() {
-//     const filteredContacts = this.getVisibleContacts();
-
-//     return (
-//       <div>
-//         <Container title={'Phonebook'}>
-//           <ContactForm onSubmit={this.addContact} />
-//         </Container>
-
-//         <Container title={'Contacts'}>
-//           <Filter
-//             value={this.state.filter}
-//             onChange={this.hendleChangeFilter}
-//           />
-//           <ContactList
-//             contacts={filteredContacts}
-//             deleteContact={this.deleteContact}
-//           />
-//         </Container>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,3 @@
-//========== hooks ======================
-// import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
 import { FaUserPlus, FaTty, FaUserAlt } from 'react-icons/fa';
@@ -11,13 +9,6 @@ export default function ContactForm({ onSubmit }) {
   const [number, setNumber] = useLocalStorage('number', '');
   const nameRandomId = nanoid(10);
   const numberRandomId = nanoid(10);
-
-  // const hendleNameChange = e => {
-  //   setName(e.target.value);
-  // };
-  // const hendleNumberChange = e => {
-  //   setNumber(e.target.value);
-  // };
 
   const hendleChange = e => {
     // console.log(e.target.name);
@@ -93,93 +84,3 @@ export default function ContactForm({ onSubmit }) {
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
-//==========================================
-// import React, { Component } from 'react';
-// import { nanoid } from 'nanoid';
-// import PropTypes from 'prop-types';
-// import { FaUserPlus, FaTty, FaUserAlt } from 'react-icons/fa';
-// import css from './ContactForm.module.css';
-
-// class ContactForm extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   nameRandomId = nanoid(10);
-//   numberRandomId = nanoid(10);
-
-//   hendleChange = event => {
-//     const { name, value } = event.currentTarget;
-
-//     this.setState({ [name]: value });
-
-//     // console.log(event.currentTarget.value);
-//   };
-
-//   formSubmit = event => {
-//     event.preventDefault();
-
-//     this.props.onSubmit(this.state);
-//     // в консоль при кліку на сабміт при тому, коли в App.User CL(data):
-//     // console.log(this.state);
-//     // console.log(this.state);
-
-//     this.resetForm();
-//   };
-
-//   resetForm = () => {
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   render() {
-//     return (
-//       // input.name must be exactly as in state
-
-//       <form className={css.contacts} action="" onSubmit={this.formSubmit}>
-//         <label className={css.input} htmlFor={this.nameRandomId}>
-//           <FaUserAlt width={160} height={160} />
-//           <span className={css.inputName}>Name:</span>
-//           <input
-//             className={css.inputField}
-//             type="text"
-//             name="name"
-//             placeholder="Name Surname"
-//             id={this.nameRandomId}
-//             value={this.state.name}
-//             onChange={this.hendleChange}
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//           />
-//         </label>
-
-//         <label className={css.input} htmlFor={this.numberRandomId}>
-//           <FaTty />
-//           <span className={css.inputName}>Number:</span>
-//           <input
-//             className={css.inputField}
-//             type="tel"
-//             name="number"
-//             placeholder="555-55-55"
-//             id={this.numberRandomId}
-//             value={this.state.number}
-//             onChange={this.hendleChange}
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//           />
-//         </label>
-
-//         <button className={css.inputBtn} type="submit" aria-label="Add contact">
-//           <FaUserPlus />
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
-
-// export default ContactForm;
-
-// ContactForm.propTypes = { onSubmit: PropTypes.func.isRequired };
